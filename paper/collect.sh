7dd1f7032c4670fff1479ca78f0b97a0e97f843c
lgbouma <bouma.luke@gmail.com>
2021-05-04 15:06:31 -0700

    stage manuscript

---
+++ paper/collect.sh
@@ -0,0 +1,3 @@
+cp /Users/luke/Dropbox/proj/rudolf/results/XYZvtang/XYZvtang_show1627_showcomovers_showsun.pdf f1.pdf
+
+cp /Users/luke/Dropbox/proj/rudolf/results/skychart/skychart_showkepler_showtess_shownakedeye_showcomovers.pdf f2.pdf
