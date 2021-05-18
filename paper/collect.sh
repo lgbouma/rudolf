e26f65858b581d1b1d8a8389f7bef851c3ffaa4c
lgbouma <bouma.luke@gmail.com>
2021-05-18 12:22:25 -0700

    update ms

--- paper/collect.sh
+++ paper/collect.sh
@@ -2,4 +2,8 @@ cp /Users/luke/Dropbox/proj/rudolf/results/XYZvtang/XYZvtang_show1627_showcomove
 
 cp /Users/luke/Dropbox/proj/rudolf/results/skychart/skychart_showkepler_showtess_shownakedeye_showcomovers.pdf f2.pdf
 
+cp /Users/luke/Dropbox/proj/rudolf/results/keplerlc/keplerlc_Nsample500_xlim0500_0600.pdf f4.pdf
+
 cp /Users/luke/Dropbox/proj/rudolf/results/run_gptransit/Kepler_1627_gptransit_posterior_phaseplot.pdf f5.pdf
+
+
