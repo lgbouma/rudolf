fa581dea526028fa7575bcb6c4211c6782d52f18
lgbouma <bouma.luke@gmail.com>
2021-09-24 15:56:05 -0700

    clean up paper directory

--- paper/collect.sh
+++ paper/collect.sh
@@ -1,49 +1,49 @@
-cp /Users/luke/Dropbox/proj/rudolf/results/XYZvtang/XYZvtang_show1627_showcomovers_showsun.pdf f1.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/skychart/skychart_showkepler_showtess_shownakedeye_showcomovers.pdf f1.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/skychart/skychart_showkepler_showtess_shownakedeye_showcomovers.pdf f2.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_IC_2602_Pleiades_cleanhrcut_gaia2018_overplotkep1627.pdf f2a.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_IC_2602_Pleiades_cleanhrcut_gaia2018_smalllims_overplotkep1627.pdf f2b.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/rotation/deltaLyrCluster_rotation_vs_BpmRp_talkaspect_kinematicspatialselected_linear_curtiscleaning_overplotkep1627.pdf f2c.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/keplerlc/keplerlc_Nsample500_xlim0500_0600.pdf f4.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/keplerlc/keplerlc_Nsample500_xlim0500_0600.pdf f3a.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_posterior_phaseplot.pdf f3b.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_posterior_phaseplot_fullxlim.pdf f3c.pdf
+
+cp /Users/luke/Dropbox/proj/rudolf/results/ttv_vs_local_slope/ttv_vs_local_slope.pdf f4a.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/RM/quadraticfreejitter/rm_RV_and_phot_showmodelbands.pdf f4b.pdf
+
+cp ~/Dropbox/proj/rudolf/results/rp_vs_period_scatter/rp_vs_period_scatter_20210915_colorbyage_showkep1627.pdf f5.pdf
+
+##########
+# figure set
 
 cp /Users/luke/Dropbox/proj/rudolf/results/keplerlc/*.pdf FigureSetKeplerLC/.
 
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_posterior_phaseplot.pdf f5a.pdf
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_posterior_phaseplot_fullxlim.pdf f5b.pdf
+##########
+# appendices
 
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_cornerplot.png dontinclude0.png
+cp /Users/luke/Dropbox/proj/rudolf/results/XYZvtang/XYZvtang_show1627_showcomovers_showsun.pdf f6.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/gaia_ruwe_vs_apparentmag/ruwe_vs_apparentmag.pdf f6.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/rotation/deltaLyrCluster_rotation_vs_BpmRp_emphbinaries_talkaspect_kinematicspatialselected_linear_curtiscleaning.pdf f7.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/flares/flarezoom_itergp.pdf f7.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_phasedsubsets_quarterchunk.pdf f8.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/flares/flarephase_itergp_altailabel.pdf f8.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_phasedsubsets_yearchunk.pdf f9.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/flares/flare_pair_separation_histogram_poisson_real_and_1000sample_average.pdf f13.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/phasedlc_slope_quartiles/Kepler_1627_RotGPtransit_phasedlc_slope_quartiles.pdf f10.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_phasedsubsets_quarterchunk.pdf f9.pdf
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_phasedsubsets_yearchunk.pdf f10.pdf
-cp /Users/luke/Dropbox/proj/rudolf/results/phasedlc_slope_quartiles/Kepler_1627_RotGPtransit_phasedlc_slope_quartiles.pdf f19.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/imaging/imaging_summary.pdf f11a.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/gaia_ruwe_vs_apparentmag/ruwe_vs_apparentmag.pdf f11b.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/ttv_vs_local_slope/ttv_vs_local_slope.pdf f11.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/rvactivitypanel/rvactivitypanel.pdf f12.pdf
 
-cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit_Kepler_1627_Q15slc/Kepler_1627_Q15slc_RotGPtransit_posterior_phaseplot.pdf f12.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/flares/flarezoom_itergp.pdf f13.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/flares/flare_pair_separation_histogram_poisson_real_and_1000sample_average.pdf f14.pdf
 
-cp ~/Dropbox/proj/rudolf/results/rp_vs_period_scatter/rp_vs_period_scatter_20210915_colorbyage_showkep1627.pdf f14.pdf
+##########
 
-
-cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_IC_2602_Pleiades_cleanhrcut_gaia2018_overplotkep1627.pdf f15a.pdf
-cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_IC_2602_Pleiades_cleanhrcut_gaia2018_smalllims_overplotkep1627.pdf f15b.pdf
-
-cp /Users/luke/Dropbox/proj/rudolf/results/rotation/deltaLyrCluster_rotation_vs_BpmRp_talkaspect_kinematicspatialselected_linear_curtiscleaning_overplotkep1627.pdf f15c.pdf
-
-cp /Users/luke/Dropbox/proj/rudolf/results/rotation/deltaLyrCluster_rotation_vs_BpmRp_emphbinaries_talkaspect_kinematicspatialselected_linear_curtiscleaning.pdf f15supp.pdf
-
-cp /Users/luke/Dropbox/proj/rudolf/results/RM/quadraticfreejitter/rm_RV_and_phot_showmodelbands.pdf f16.pdf
-
-
-cp /Users/luke/Dropbox/proj/rudolf/results/imaging/imaging_summary.pdf f17.pdf
-
-cp /Users/luke/Dropbox/proj/rudolf/data/imaging/Kepler1627_20210624_562_832_final.pdf f17a.pdf
-cp /Users/luke/Dropbox/proj/rudolf/data/imaging/KOA_104524/NIRC2/calibrated/N2.20150722.39302_drp.jpg f17b.jpg
-
-
-cp /Users/luke/Dropbox/proj/rudolf/results/rvactivitypanel/rvactivitypanel.pdf f18.pdf
+# #the figures that weren't
+#cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit/Kepler_1627_RotGPtransit_cornerplot.png dontinclude0.png
+#cp /Users/luke/Dropbox/proj/rudolf/results/flares/flarephase_itergp_altailabel.pdf f8.pdf
+#cp /Users/luke/Dropbox/proj/rudolf/results/run_RotGPtransit_Kepler_1627_Q15slc/Kepler_1627_Q15slc_RotGPtransit_posterior_phaseplot.pdf f12.pdf
+#cp /Users/luke/Dropbox/proj/rudolf/data/imaging/Kepler1627_20210624_562_832_final.pdf f17a.pdf
+#cp /Users/luke/Dropbox/proj/rudolf/data/imaging/KOA_104524/NIRC2/calibrated/N2.20150722.39302_drp.jpg f17b.jpg
