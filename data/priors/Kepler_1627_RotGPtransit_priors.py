bd1dff34d062a15cac0095fe480209cbf7b6d36f
lgbouma <bouma.luke@gmail.com>
2021-10-06 13:49:21 -0700

    update RotGPtransit prior
    Mean is now zero-centered, as it should be
    We now fit for log depth, not log Rp/R*

--- data/priors/Kepler_1627_RotGPtransit_priors.py
+++ data/priors/Kepler_1627_RotGPtransit_priors.py
@@ -12,14 +12,16 @@ import numpy as np
 priordict = {
 'period': ('Normal', 7.20280608, 0.01), # Holczer+16
 't0': ('Normal', 120.790531, 0.02), # Holczer+16, 2454953.790531 - 2454833 for Kepler timestamp
-'log_r': ('Uniform', np.log(1e-2), np.log(1), np.log(0.0433)),
-'b': ('ImpactParameter', 0.5),
+#'log_r': ('Uniform', np.log(1e-2), np.log(1), np.log(0.0433)),
+#'b': ('ImpactParameter', 0.5),
+'log_depth': ('Normal', np.log(1.8e-3), 2),
+'b': ('Uniform', 0, 1),
 'u_star': ('QuadLimbDark',),
 #'u[0]': ('Uniform', 0.51-0.2, 0.51+0.2, 0.51), # Claret+2011, Teff 5500K, logg 4.5, solar metallicity, V-band
 #'u[1]': ('Uniform', 0.24-0.2, 0.24+0.2, 0.240),
 'r_star': ('TruncatedNormal', 0.881, 0.018), # Cluster isochrone (MIST+PARSEC)
 'logg_star': ('Normal', 4.53, 0.05), # Cluster isochrone (MIST+PARSEC)
-'mean': ('Normal', 0.5, 0.1),
+'mean': ('Normal', 0, 0.1),
 'ecc': ('EccentricityVanEylen19',),
 'omega': ('Uniform', 0, 2*np.pi),
 'log_jitter': ('Normal', r"\log\langle \sigma_f \rangle", 2.0),
