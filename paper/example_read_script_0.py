587b74c0b6405cfd81fe3867cfbef90cba700b7e
lgbouma <bouma.luke@gmail.com>
2022-01-03 13:30:36 -0800

    add example read scripts for Table 3

---
+++ paper/example_read_script_0.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+This example script reads Table 3 from Bouma+2022, also known as v0.6 of the
+CDIPS target list.  After reading the table using python's pandas library, we
+show what the table contains for some stars in the Pleiades.
+
+Author: Luke Bouma
+Contact: <luke@astro.caltech.edu>
+"""
+
+import pandas as pd, numpy as np
+
+# Read in the table of young, age-dated, and age-dateable stars using the
+# pandas DataFrame type.  Note that astropy.table's Table.read function would
+# work here as well -- the parser just needs to be able to distinguish
+# comma-separated strings from the comma separation in the CSV table itself.
+df = pd.read_csv("cdips_targets_v0.6_nomagcut_gaiasources_table.csv")
+
+# Select only stars that are actually reported to be in a cluster, rather than
+# field stars thought to be young.
+sel = ~pd.isnull(df.cluster)
+sdf = df[sel]
+
+# How many unique cluster name strings are there? Note: if a given star is
+# reported by multiple literature sources to be in a cluster, the "cluster",
+# "age", "reference_id", and "reference_bibcode" columns are all comma
+# separated strings.
+
+# Assuming Python3 is being run, print using f-strings.
+N_stars = len(sdf)
+N_uniq = len(np.unique(sdf.cluster))
+print(42*'.')
+print(f"There are {N_stars} stars reported to be in clusters.")
+print(f"These stars have {N_uniq} unique cluster name strings.")
+
+# Search for stars containing the string "Pleiades".  Note that this selection
+# is not complete!  Other possible strings to match on include "Melotte_22",
+# "Melotte 22", and more.  The best way to resolve all possibilities if you
+# have a particular cluster for which you want all possible candidates would be
+# to use a function that finds all the unique strings (like np.unique).
+clustername = 'Pleiades'
+sel = sdf.cluster.str.contains(clustername)
+_df = sdf[sel]
+print(42*'.')
+print(
+    f"Searching for stars containing the string {clustername} "
+    f"yields {len(_df)} stars, including the following."
+)
+
+selcols = 'dr2_source_id,cluster,age,reference_id,reference_bibcode'.split(',')
+print(_df[selcols].sample(n=20))
+
+print(42*'.')
+print("Let's look at Gaia DR2 65112739296356608 closer...")
+
+for c in selcols:
+    source_id = np.int64(65112739296356608)
+    print(f"{c}: {_df.loc[_df.dr2_source_id == source_id, c].iloc[0]}")
+
+print('So each of the cluster, age, reference_id, and reference_bibcode '
+      'columns is comma-separated to account for different reports from the '
+      'literature.')
+print(42*'.')
+
+print('For a second manipulation example using pandas, see '
+      'example_read_script_1.py')
---
+++ paper/example_read_script_1.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+This example script reads Table 3 from Bouma+2022, and then uses the pandas
+boolean logic operators to select a set of interesting open clusters in the
+northern celestial hemisphere studied by Kounkel & Covey 2019.
+
+Author: Luke Bouma
+Contact: <luke@astro.caltech.edu>
+"""
+import pandas as pd
+import numpy as np
+
+# Read in the table of young, age-dated, and age-dateable stars using the
+# pandas DataFrame type. Select only stars that are actually reported to be in
+# a cluster, rather than field stars thought to be young.
+df = pd.read_csv("cdips_targets_v0.6_nomagcut_gaiasources_table.csv")
+df = df[~pd.isnull(df.cluster)]
+
+# In this example, we will select clusters from the union of "cluster_names"
+# and "special_names".  As in the first example, this selection is not complete
+# for any given cluster.  For the Pleiades for instance; other possible strings
+# to match on include "Melotte_22", "Melotte 22", would yield more members.
+# The best way to resolve all possibilities if you have a particular cluster
+# for which you want all possible candidates would be to use a function that
+# finds all the unique strings (like np.unique).
+
+# NOTE: these names come from Kounkel & Covey 2019.  Other authors may have
+# used different names to refer to the same clusters.
+cluster_names = [
+    "Stephenson_1", # logt 7.5, 100-500pc, aka. Theia 73
+    "Alpha_per", # logt 7.8, 100-200pc, Theia 133
+    "kc19group_506", # 350 Myr, 100pc, aka. Theia 506
+    "kc19group_507", # 350 Myr, 100-200pc, aka. Theia 507.
+    "AB_Dor", # ~120 Myr, 100-300pc from KC19.
+    "Pleiades", # 120 Myr, 120 pc.
+]
+special_names = [
+    "UBC_1", # 350 Myr, 300-400pc, aka. Theia 520
+    "RSG_5", # logt 7.5, 300-400pc.
+]
+
+sel = np.zeros(len(df)).astype(bool)
+for cluster_name in cluster_names:
+    sel |= df.cluster.str.contains(cluster_name)
+
+# Avoid e.g., "UBC_186" matching for the clusters "UBC_1", "UBC_10", "UBC_11",
+# etc. when applying the string matching.
+for cluster_name in special_names:
+    sel |= (
+        # This selects singleton cases
+        (df.cluster == cluster_name)
+        |
+        # This selects cases that contain the string "UBC_1,", i.e., they begin
+        # with that name.
+        df.cluster.str.contains(cluster_name+',')
+        |
+        # This selects cases that contain the string ",UBC_1,", i.e., UBC_1 is
+        # somewhere in the middle of the string.
+        df.cluster.str.contains(','+cluster_name+',')
+        |
+        # This selects cases that end with the string "UBC_1".  Note that
+        # pandas matches the pattern as a regular expression, see
+        # https://pandas.pydata.org/docs/reference/api/pandas.Series.str.contains.html
+        df.cluster.str.contains(','+cluster_name+'$')
+    )
+
+np.random.seed(42)
+sdf = df[sel]
+selcols = 'dr2_source_id,cluster,mean_age,age,reference_id,reference_bibcode'.split(',')
+N = 30
+
+print(f'Selecting stars matching {cluster_names} and {special_names}...')
+print(f'Yields {len(sdf)} stars')
+print(f'A random sampling of N={N} them is as follows:')
+print(sdf[selcols].sample(n=N))
