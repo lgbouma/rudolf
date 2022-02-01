036752ed483a46bbfaa1e9e5f7aa0fa2531100cc
lgbouma <bouma.luke@gmail.com>
2022-01-31 16:28:06 -0800

    tweak RotGPtransit driver for other KOIs

--- drivers/run_RotGPtransit.py
+++ drivers/run_RotGPtransit.py
@@ -1,14 +1,12 @@
 """
-Fit the Kepler 1627 dataset, using a GP for the stellar variability and a
-transit for the planet.
+Fit the Kepler-1627 / Kepler-1643 / KOI-7368 / KOI-7913 datasets, using a GP
+for the stellar variability and a transit for the planet.
 """
 
 import numpy as np, matplotlib.pyplot as plt, pandas as pd, pymc3 as pm
-import pickle, os, corner, pytest
-from collections import OrderedDict
-import exoplanet as xo
-
+import os
 from os.path import join
+from collections import OrderedDict
 from importlib.machinery import SourceFileLoader
 
 try:
@@ -27,10 +25,11 @@ from rudolf.helpers import (
 from rudolf.paths import DATADIR, RESULTSDIR
 
 # NOTE: change starid as desired based on the dataset to use.
-# Kepler_1627_Q15slc, or Kepler_1627
 #def run_RotGPtransit(starid='Kepler_1627_Q15slc', N_samples=2000):
 #def run_RotGPtransit(starid='Kepler_1627', N_samples=2000):
-def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
+#def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
+def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
+#def run_RotGPtransit(starid='Kepler_1643', N_samples=1000):
 
     assert starid in ['Kepler_1627', 'Kepler_1627_Q15slc', 'KOI_7368',
                       'KOI_7913', 'Kepler_1643']
@@ -88,7 +87,7 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
     # map_optimization_method = 'RotationTerm_transit'
     # #does not work (produces grazing transit)
     # map_optimization_method = 'transit_gpJitterMean_FullOptimize'
-    # #works
+    # #works for Kepler_1627, KOI_7368
     map_optimization_method = 'RotationTerm_transit_FullOptimize'
     m = ModelFitter(modelid, datasets, priordict, plotdir=PLOTDIR,
                     pklpath=pklpath, overwrite=0, N_samples=N_samples,
@@ -103,7 +102,7 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
             'a_Rs', 'cosi', 'sini','T_14','T_13'
         ]
     elif parametrization == 'log_ror_and_b':
-        #NOTE: if in log_ror + b parametrization (to explore grazing), depth is not derived
+        # if in log_ror + b parametrization (to explore grazing), depth is not derived
         var_names = [
             'mean','logg_star','r_star','t0','period', 'log_ror', 'b', 'ecc',
             'omega','u_star', 'log_jitter', 'log_prot','log_Q0','log_dQ',
@@ -124,6 +123,20 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
     phasedsubsets = 1
     getbecclimits = 1
 
+    if cornerplot:
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_cornerplot.png')
+        if starid == 'KOI_7913':
+            # don't plot T_13 due to NaN-based convergence issues.
+            corner_var_names = var_names[:-1]
+        else:
+            corner_var_names = var_names
+        bp.plot_cornerplot(corner_var_names, m, outpath)
+
+    if posttable:
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_posteriortable.tex')
+        make_posterior_table(pklpath, priordict, outpath, modelid, makepdf=1,
+                             var_names=var_names)
+
     if writevespa:
         from rudolf.vespa import _write_vespa
         staridentifier = f'{starid}_{modelid}'
@@ -134,6 +147,8 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
         outpath = join(PLOTDIR, f'{starid}_{modelid}_posterior_phaseplot.png')
         if starid == 'KOI_7368':
             ylimd = {'A':[-2, 1.5], 'B':[-0.19,0.19]}
+        elif starid == 'KOI_7913':
+            ylimd = {'A':[-1, 0.5], 'B':[-0.05,0.05]}
         else:
             ylimd = {'A':[-3.5, 2.5], 'B':[-0.19,0.19]}
         alpha = 1 # 3e-1 looks good
@@ -157,11 +172,6 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
         get_becc_limits(
             datasets, m.trace.posterior
         )
-
-    if posttable:
-        outpath = join(PLOTDIR, f'{starid}_{modelid}_posteriortable.tex')
-        make_posterior_table(pklpath, priordict, outpath, modelid, makepdf=1,
-                             var_names=var_names)
 
     if phasedsubsets:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_phasedsubsets_yearchunk.png')
@@ -187,10 +197,6 @@ def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
             map_estimate=m.map_estimate, yoffsetNsigma=3.5, ylimd=ylimd,
             inch_per_subset=0.35, binsize_minutes=20
         )
-
-    if cornerplot:
-        outpath = join(PLOTDIR, f'{starid}_{modelid}_cornerplot.png')
-        bp.plot_cornerplot(var_names, m, outpath)
 
 
 if __name__ == "__main__":
