9ee4aaea2f96862d0e62b6530b8b2fd03f7e3dbd
lgbouma <bouma.luke@gmail.com>
2022-02-16 23:46:27 -0500

    add run_simpletransit_polyremover

--- drivers/run_simpletransit_polyremover.py
+++ drivers/run_simpletransit_polyremover.py
@@ -26,20 +26,20 @@ from rudolf.helpers import (
 from rudolf.paths import DATADIR, RESULTSDIR
 
 EPHEMDICT = {
-    'KOI_7913': {'t0': 2454987.513-2454833, 'per': 24.2783801, 'tdur':4.564/24},
+    'KOI_7368': {'t0': 2454970.06-2454833, 'per': 6.842939, 'tdur':2.8/24, 'n_tdurs':3.5},
+    'KOI_7913': {'t0': 2454987.513-2454833, 'per': 24.2783801, 'tdur':4.564/24, 'n_tdurs':2.5},
 }
 
 # NOTE: change starid as desired based on the dataset to use.
 #def run_RotGPtransit(starid='Kepler_1627_Q15slc', N_samples=2000):
 #def run_RotGPtransit(starid='Kepler_1627', N_samples=2000):
-#def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
-def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
+def run_RotGPtransit(starid='KOI_7368', N_samples=2000):
+#def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
 #def run_RotGPtransit(starid='Kepler_1643', N_samples=1000):
 
-    assert starid in ['Kepler_1627', 'Kepler_1627_Q15slc', 'KOI_7368',
-                      'KOI_7913', 'Kepler_1643']
+    assert starid in ['Kepler_1627', 'KOI_7368', 'KOI_7913', 'Kepler_1643']
 
-    modelid = 'localpolytransit'
+    modelid = 'simpletransit'
 
     PLOTDIR = os.path.join(RESULTSDIR, f'run_{modelid}_{starid}')
     if not os.path.exists(PLOTDIR):
@@ -49,10 +49,6 @@ def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
     if starid == 'Kepler_1627':
         time, flux, flux_err, qual, texp = (
             get_manually_downloaded_kepler_lightcurve(lctype='longcadence')
-        )
-    elif starid == 'Kepler_1627_Q15slc':
-        time, flux, flux_err, qual, texp = (
-            get_manually_downloaded_kepler_lightcurve(lctype='shortcadence')
         )
     elif starid in ['KOI_7368', 'KOI_7913', 'Kepler_1643']:
         time, flux, flux_err, qual, texp = (
@@ -65,41 +61,30 @@ def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
     sel = (qual == 0)
     time, flux, flux_err, texp = time[sel], flux[sel], flux_err[sel], texp
 
-    # trim
-    outpath = join(PLOTDIR, f'{starid}_rawlc.png')
-    _quicklcplot(time, flux, flux_err, outpath)
-    n_tdurs = 4.0
-    time, flux, flux_err = _subset_cut(
-        time, flux, flux_err, n=n_tdurs, t0=EPHEMDICT[starid]['t0'],
-        per=EPHEMDICT[starid]['per'], tdur=EPHEMDICT[starid]['tdur']
+    from cdips.lcproc.detrend import transit_window_polynomial_remover
+    outpath = join(PLOTDIR, f'{starid}_{modelid}.png')
+    d = transit_window_polynomial_remover(
+        time, flux, flux_err, EPHEMDICT[starid]['t0'],
+        EPHEMDICT[starid]['per'], EPHEMDICT[starid]['tdur'],
+        n_tdurs=EPHEMDICT[starid]['n_tdurs'],
+        method='poly_4', plot_outpath=outpath
     )
-    outpath = join(PLOTDIR, f'{starid}_rawtrimlc.png')
-    _quicklcplot(time, flux, flux_err, outpath)
 
-    # build datasets
-    mingap = EPHEMDICT[starid]['per'] - 3*n_tdurs*EPHEMDICT[starid]['tdur']
-    assert mingap > 0
-    ngroups, groupinds = find_lc_timegroups(time, mingap=mingap)
+    datasets = OrderedDict()
 
-    for ix, g in enumerate(groupinds):
-        tess_texp = np.nanmedian(np.diff(time[g]))
-        datasets[f'kepler_{ix}'] = [time[g], flux[g], flux_err[g], tess_texp]
+    time = np.hstack([d[f'time_{ix}'] for ix in range(d['ngroups'])])
+    flux = np.hstack([d[f'flat_flux_{ix}'] for ix in range(d['ngroups'])])
+    flux_err = np.hstack([d[f'flux_err_{ix}'] for ix in range(d['ngroups'])])
+    texp = np.nanmedian(np.diff(time))
 
-    # get / make prior
+    datasets['kepler'] = [time, flux, flux_err, texp]
+
+    # get prior
     priorpath = join(DATADIR, 'priors', f'{starid}_{modelid}_priors.py')
     if not os.path.exists(priorpath):
         raise FileNotFoundError(f'need to create {priorpath}')
     priormod = SourceFileLoader('prior', priorpath).load_module()
-    _init_priordict = priormod.priordict
-    priordict = deepcopy(_init_priordict)
-
-    if modelid == 'localpolytransit' and 'kepler_0_mean' not in priordict.keys():
-        for n, (name, (x, y, yerr, texp)) in enumerate(datasets.items()):
-            # mean + a1*(time-midtime) + a2*(time-midtime)^2.
-            priordict[f'{name}_mean'] = ('Normal', 0, 0.1)
-            priordict[f'{name}_a1'] = ('Uniform', -0.1, 0.1)
-            priordict[f'{name}_a2'] = ('Uniform', -0.1, 0.1)
-    given_priordict_make_priorfile(priordict, priorpath)
+    priordict = priormod.priordict
 
     pklpath = join(BETTYDIR, f'run_{starid}_{modelid}.pkl')
 
@@ -113,27 +98,28 @@ def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
                         kind='stats', stat_funcs={'median':np.nanmedian},
                         extend=True)
 
-    writevespa = 1
+    localpolyindivpanels = 1
     phaseplot = 1
-    cornerplot = 1
     posttable = 1
-    phasedsubsets = 1
+    cornerplot = 1
+    writevespa = 1
     getbecclimits = 1
-    phasesampleplot = 1
+
+    if localpolyindivpanels:
+        outpath = join(PLOTDIR, f'{starid}_{modelid}_localpolyindivpanels.png')
+        bp.plot_localpolyindivpanels(d, m, summdf, outpath, modelid=modelid)
 
     if phaseplot:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_phaseplot.png')
+        if starid == 'KOI_7368':
+            ylimd = {'A':[-2, 1.5], 'B':[-0.19,0.19]}
+        elif starid == 'KOI_7913':
+            ylimd = {'A':[-1.5, 0.5], 'B':[-0.05,0.05]}
+        else:
+            ylimd = {'A':[-3.5, 2.5], 'B':[-0.19,0.19]}
         bp.plot_phasefold(m, summdf, outpath, modelid=modelid, inppt=1,
-                          binsize_minutes=15, singleinstrument='tess')
-
-    if fitindivpanels:
-        outpath = join(PLOTDIR, f'{starid}_{modelid}_fitindivpanels.png')
-        bp.plot_fitindivpanels(m, summdf, outpath, modelid=modelid,
-                               singleinstrument='tess')
-
-    if posttable:
-        outpath = join(PLOTDIR, f'{starid}_{modelid}_trimmed_posteriortable.tex')
-        make_posterior_table(pklpath, _init_priordict, outpath, modelid, makepdf=1)
+                          ylimd=ylimd, binsize_minutes=15,
+                          singleinstrument='kepler')
 
     if posttable:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_posteriortable.tex')
@@ -141,11 +127,9 @@ def run_RotGPtransit(starid='KOI_7913', N_samples=2000):
 
     if cornerplot:
         outpath = join(PLOTDIR, f'{starid}_{modelid}_trimmed_cornerplot.png')
-        bp.plot_cornerplot(list(_init_priordict), m, outpath)
+        bp.plot_cornerplot(list(priordict), m, outpath)
 
     if writevespa:
-        # TODO TODO TODO FIXME FIXME: needs re-implementation with this
-        # localpolytransit model...
         from rudolf.vespa import _write_vespa
         staridentifier = f'{starid}_{modelid}'
         _write_vespa(datasets, m.trace.posterior, staridentifier,
