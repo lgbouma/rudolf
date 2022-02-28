a363f2979bfb8654f2ac59012d3711fe663723b4
lgbouma <bouma.luke@gmail.com>
2022-02-28 11:26:11 -0800

    stage Cep-Her outline

---
+++ papers/collect.sh
@@ -0,0 +1,20 @@
+# Cep-Her XYZ
+cp /Users/luke/Dropbox/proj/rudolf/results/XYZvtang/XYZvtang_show1627_showallknown_showrsg5_showsun_square.pdf f1.pdf
+
+# rotation, CAMD, spectra (Li/H-alpha)
+# CAMD:
+cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_Set1_IC_2602_Pleiades_cleanhrcut_gaia2018_overplotkep1627_allknownkois.pdf f2a.pdf
+cp /Users/luke/Dropbox/proj/rudolf/results/hr/hr_Bp_m_Rp_show100pc_redcorr_δ_Lyr_cluster_Set1_IC_2602_Pleiades_cleanhrcut_gaia2018_smalllims_overplotkep1627_allknownkois.pdf f2b.pdf
+# rotation:
+cp /Users/luke/Dropbox/proj/rudolf/results/rotation/deltaLyrCluster_rotation_vs_BpmRp_talkaspect_kinematicspatialselected_linear_curtiscleaning_overplotkep1627.pdf f2c.pdf
+# spectra: Li EW vs Teff and Halpha EW vs Teff
+#TODO TODO
+
+# phased and sliced LCs
+cp ~/Dropbox/proj/rudolf/results/run_simpletransit_Kepler_1643/*phase*png f3a.png
+cp ~/Dropbox/proj/rudolf/results/run_simpletransit_KOI_7913/*phase*png f3b.png
+cp ~/Dropbox/proj/rudolf/results/run_simpletransit_KOI_7368/*phase*png f3c.png
+cp ~/Dropbox/proj/rudolf/results/run_simpletransit_Kepler_1627/*phase*png f3d.png
+
+# Rp, Period, Age
+cp ~/Dropbox/proj/rudolf/results/rp_vs_period_scatter/rp_vs_period_scatter_20220227_colorbyage_showCepHer.pdf f4.pdf
